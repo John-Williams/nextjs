a4469c7e331c4f42888b22f1b913c917c50f3de9
John Williams <jcwilliams@gmail.com>
2021-07-20 12:56:20 +0100

    updating next.config file

--- next.config.js
+++ next.config.js
@@ -1,16 +1,6 @@
-{
-  "name": "nextjs",
-  "version": "0.1.0",
-  "private": true,
-  "scripts": {
-    "dev": "next dev",
-    "build": "next build",
-    "start": "next start"
-  },
-  "dependencies": {
-    "@auth0/nextjs-auth0": "^1.3.0",   
-    "next": "10.x",
-    "react": "17.x",
-    "react-dom": "17.x"
-  }
+
+const nextConfig = {
+  webpack5: false
 }
+
+module.exports = nextConfig
